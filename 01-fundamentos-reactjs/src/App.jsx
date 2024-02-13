ac5b58848d61a97f8dc23f7ce3c023cc520737c0
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-02-12 22:18:32 -0300

    Estrutura da aplicação: CSS Global

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,14 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
-import './styles.css'
+import './global.css'
 
 export function App() {
 
   return (
     <div>
       <Header />
-      
+
       <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda alias quas eveniet aspernatur. Esse laboriosam doloribus a! Quo placeat facere amet vel ipsam quasi itaque cumque quas. Ea, sapiente inventore." 
