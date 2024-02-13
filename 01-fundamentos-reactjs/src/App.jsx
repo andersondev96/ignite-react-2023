28911276f94f6924d73e4f13089636021d8ec1c2
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-02-12 22:05:24 -0300

    Estrutura da aplicação: CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,25 @@
-function App() {
+import { Post } from './Post'
+import { Header } from './components/Header'
+
+import './styles.css'
+
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      
+      <Post 
+        author="Diego Fernandes" 
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda alias quas eveniet aspernatur. Esse laboriosam doloribus a! Quo placeat facere amet vel ipsam quasi itaque cumque quas. Ea, sapiente inventore." 
+      />
+
+      <Post 
+        author="Gabriel Buzi" 
+        content="Novo post muito legal." 
+      />
+    </div>
   )
 }
 
-export default App
+
