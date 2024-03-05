40571155662bb12b4ae7d08efe7f01e6e40ce9e0
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-03-05 19:11:53 -0300

    Os motores do React - Entendendo a key

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -53,9 +53,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type === 'link') {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p ket={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -76,8 +76,8 @@ export function Post({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                {comments.map(comment => {
-                    return <Comment content={comment} />
+                {comments.map((comment) => {
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
         </article>
