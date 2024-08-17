461d4a42df653101067e4390e6dcd0278dcdfb92
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-08-17 19:52:31 -0300

    Integração com Stripe - Formatar preço

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -6,7 +6,6 @@ import { useKeenSlider } from 'keen-slider/react'
 import { stripe } from "../lib/stripe"
 import { HomeContainer, Product } from "../styles/pages/home"
 
-
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe"
 
@@ -56,7 +55,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',  
+      }).format(price.unit_amount / 100),
     }
   })
 
