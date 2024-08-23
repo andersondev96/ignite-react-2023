ece71ec9e7c19b16bb0c1da070bb04dbe8fd8ed8
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-08-22 21:54:42 -0300

    Produto & Checkout - Checkout no stripe

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,6 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 import Stripe from 'stripe'
 import { stripe } from '../../lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
@@ -12,16 +11,13 @@ interface ProductProps {
     imageUrl: string;
     price: string;
 		description: string;
+		defaultPriceId: string;
 	}
 }
 
 export default function Product({ product }: ProductProps) {
-	const { isFallback } = useRouter()
-
-	if (isFallback) {
-		return (
-			<p>Loading...</p>
-		)
+	function handleBuyProduct() {
+		console.log(product.defaultPriceId);
 	}
 
 	return (
@@ -35,7 +31,7 @@ export default function Product({ product }: ProductProps) {
 
 				<p>{product.description}</p>
 
-				<button>
+				<button onClick={handleBuyProduct}>
 					Comprar agora
 				</button>
 			</ProductDetails>
@@ -74,7 +70,8 @@ export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params
 					style: 'currency',
 					currency: 'BRL',
 				}).format(price.unit_amount / 100),
-				description: product.description
+				description: product.description,
+				defaultPriceId: price.id,
 			}
 		},
 		 revalidate: 60 * 60 * 1 // 1 hour
