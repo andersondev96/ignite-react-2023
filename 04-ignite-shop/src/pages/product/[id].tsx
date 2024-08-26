8c9ed5a793606a0d51dacf265fad73b6a8547f8a
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-08-26 16:23:35 -0300

    Produto & Checkout - Trabalhando com SEO

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 import Stripe from 'stripe'
@@ -41,6 +42,10 @@ export default function Product({ product }: ProductProps) {
 	}
 
 	return (
+		<>
+		<Head >
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
 		<ProductContainer>
 			<ImageContainer>
 				<Image src={product.imageUrl} width={520} height={480} alt="" />
@@ -56,6 +61,7 @@ export default function Product({ product }: ProductProps) {
 				</button>
 			</ProductDetails>
 		</ProductContainer>
+		</>
 	)
 }
 
