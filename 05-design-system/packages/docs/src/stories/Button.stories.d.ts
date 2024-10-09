f3ba6c937efebd9bed6d08c05ca13ba2917e51ab
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-10-08 23:00:27 -0300

    Design System em React - Configurando Turbo Repo

---
+++ 05-design-system/packages/docs/src/stories/Button.stories.d.ts
@@ -0,0 +1,7 @@
+import { ButtonProps } from '@ignite-ui/react';
+import { Meta, StoryObj } from '@storybook/react';
+declare const _default: Meta<ButtonProps>;
+export default _default;
+export declare const Primary: StoryObj<ButtonProps>;
+export declare const Big: StoryObj;
+//# sourceMappingURL=Button.stories.d.ts.map
---
+++ 05-design-system/packages/docs/src/stories/Button.stories.js
@@ -0,0 +1,14 @@
+import { Button } from '@ignite-ui/react';
+export default {
+    title: 'Form/Button',
+    component: Button,
+    args: {
+        children: 'Enviar',
+    }
+};
+export const Primary = {};
+export const Big = {
+    args: {
+        size: 'big',
+    }
+};
