83ae5c2cbc64fa5fe4a5e69e6d22d981959a4fdd
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-10-12 22:58:03 -0300

    Componentes do Design System - Componente: Box

---
+++ 05-design-system/packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,19 @@
+import { Box, BoxProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+	title: 'Surfaces/Box',
+	component: Box,
+
+	args: {
+		children: (
+			<>
+				<span>Testando o elemento Box</span>
+			</>
+		),
+	},
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {
+	
+}
--- 05-design-system/packages/docs/src/stories/Button.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { ButtonProps } from '@ignite-ui/react';
-import { Meta, StoryObj } from '@storybook/react';
-declare const _default: Meta<ButtonProps>;
-export default _default;
-export declare const Primary: StoryObj<ButtonProps>;
-export declare const Big: StoryObj;
-//# sourceMappingURL=Button.stories.d.ts.map
