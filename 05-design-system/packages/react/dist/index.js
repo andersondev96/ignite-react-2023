7934f9dbd1249da576910b0a401be971801b1b8c
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-10-13 21:43:24 -0300

    Componente: Heading

--- 05-design-system/packages/react/dist/index.js
+++ 05-design-system/packages/react/dist/index.js
@@ -38,6 +38,7 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 var src_exports = {};
 __export(src_exports, {
   Box: () => Box,
+  Heading: () => Heading,
   Text: () => Text
 });
 module.exports = __toCommonJS(src_exports);
@@ -150,6 +151,29 @@ var Box = styled("div", {
   border: "1px solid $gray600"
 });
 
+// src/components/Heading.tsx
+var Heading = styled("h2", {
+  fontFamily: "$default",
+  lineHeight: "$shorter",
+  margin: 0,
+  color: "$gray100",
+  variants: {
+    size: {
+      sm: { fontSize: "$xl" },
+      md: { fontSize: "$2xl" },
+      lg: { fontSize: "$4xl" },
+      "2xl": { fontSize: "$5xl" },
+      "3xl": { fontSize: "$6xl" },
+      "4xl": { fontSize: "$7xl" },
+      "5xl": { fontSize: "$8xl" },
+      "6xl": { fontSize: "$9xl" }
+    }
+  },
+  defaultVariants: {
+    size: "md"
+  }
+});
+
 // src/components/Text.tsx
 var Text = styled("p", {
   fontFamily: "$default",
@@ -180,5 +204,6 @@ var Text = styled("p", {
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Box,
+  Heading,
   Text
 });
