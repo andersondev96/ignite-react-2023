a481f9708e147676c813d4d442f7b6e4fc3ebb17
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-10-15 22:01:37 -0300

    Componente: TextArea

--- 05-design-system/packages/react/dist/index.mjs
+++ 05-design-system/packages/react/dist/index.mjs
@@ -304,6 +304,32 @@ var Text = styled("p", {
   }
 });
 
+// src/components/TextArea.tsx
+var TextArea = styled("textarea", {
+  backgroundColor: "$gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$white",
+  fontWeight: "$regular",
+  resize: "vertical",
+  minWidth: 80,
+  "&:focus": {
+    outline: 0,
+    borderColor: "$ignite300"
+  },
+  "&:disabled": {
+    outline: 0.5,
+    cursor: "not-allowed"
+  },
+  "&:has(input:disabled)": {
+    color: "$gray400"
+  }
+});
+
 // src/components/TextInput/styles.ts
 var TextInputContainer = styled("div", {
   backgroundColor: "$gray900",
@@ -361,5 +387,6 @@ export {
   Button,
   Heading,
   Text,
+  TextArea,
   TextInput
 };
