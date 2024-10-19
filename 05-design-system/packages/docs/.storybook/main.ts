6a932d693bd6c02c2c359cc79e1af11591e9b48a
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-10-19 15:32:58 -0300

    fix: Add subpath to Storybook on production

--- 05-design-system/packages/docs/.storybook/main.ts
+++ 05-design-system/packages/docs/.storybook/main.ts
@@ -25,6 +25,13 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: true,
+  },
+  viteFinal: (config, {configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
   }
 };
 export default config;
