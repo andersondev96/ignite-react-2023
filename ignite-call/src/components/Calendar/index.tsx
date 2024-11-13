16db41d11b166ad82ba70c06c8f48c17a8609765
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-11-12 21:55:43 -0300

    Calendário e disponibilidade - Estrutura visual do calendário

---
+++ ignite-call/src/components/Calendar/index.tsx
@@ -0,0 +1,57 @@
+import { getWeekDays } from '@/src/utils/get-week-days'
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+	CalendarActions,
+	CalendarBody,
+	CalendarContainer,
+	CalendarDay,
+	CalendarHeader,
+	CalendarTitle,
+} from './styles'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Novembro <span>2024</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekday) => (
+              <th key={weekday}>{weekday}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>
+            <CalendarDay>1</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>2</CalendarDay>
+          </td>
+          <td>
+            <CalendarDay>3</CalendarDay>
+          </td>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
